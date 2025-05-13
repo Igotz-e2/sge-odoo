d6a41e511ff4c13cb7bcfad3c9167f8d66ae23b3
Igotz-e2 <148059774+Igotz-e2@users.noreply.github.com>
2025-05-13 20:45:00 +0200

    avances

--- addons/perritos_calientes/models/models.py
+++ addons/perritos_calientes/models/models.py
@@ -1,19 +1,15 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class PerritosCalientes(models.Model):
+    _name = 'perritos_calientes.perritos_calientes'
+    _description = 'Modelo para Perritos Calientes'
 
+    name = fields.Char("Nombre")
+    value = fields.Integer("Valor")
+    value2 = fields.Float(compute="_value_pc", store=True, string="Valor %")
+    description = fields.Text("Descripción")
 
-# class perritos_calientes(models.Model):
-#     _name = 'perritos_calientes.perritos_calientes'
-#     _description = 'perritos_calientes.perritos_calientes'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
