1cb72499eb26b3585e89daa5f78803a43a202780
Igotz-e2 <148059774+Igotz-e2@users.noreply.github.com>
2025-05-13 20:13:00 +0200

    Odoo
    FUncional

--- addons/perritos_calientes/__manifest__.py
+++ addons/perritos_calientes/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
     'description': """
-Long description of module's purpose
+    Long description of module's purpose
     """,
 
     'author': "My Company",
@@ -14,7 +14,7 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Application',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
